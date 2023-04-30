c86c62e0009d9de301ebc29579bcf2534519361d
Salman Hasan <sh8salmanhasan@gmail.com>
2023-04-30 15:18:56 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3042;
+
+app.use(cors());
+app.use(express.json());
+
+const balances = {
+  '0238ad052fe7ebd7009f11c6b9aa03d6bfa74ed210ebdac0da8ca341871bf3e954': 100,
+};
+
+app.get('/balance/:address', (req, res) => {
+  const { address } = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post('/send', (req, res) => {
+  const { sender, recipient, amount } = req.body;
+
+  setInitialBalance(sender);
+  setInitialBalance(recipient);
+
+  if (balances[sender] < amount) {
+    res.status(400).send({ message: 'Not enough funds!' });
+  } else {
+    balances[sender] -= amount;
+    balances[recipient] += amount;
+    res.send({ balance: balances[sender] });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+function setInitialBalance(address) {
+  if (!balances[address]) {
+    balances[address] = 0;
+  }
+}
